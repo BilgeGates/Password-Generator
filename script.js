04db9e8c66c610099f844a706f4e3bef83fb206f
BilgeGates <xtaihuseynov131@gmail.com>
2023-08-26 10:38:42 +0400

    updated

--- script.js
+++ script.js
@@ -1,98 +1,87 @@
-const lengthSlider = document.querySelector(".head_length input");
-const options = document.querySelectorAll(".option input");
-const passwordResult = document.querySelector(".result input");
-const copyIcon = document.querySelector(".result button");
-const generateBtn = document.querySelector(".btn-large");
-const lowercaseCheckbox = document.querySelector("#lowercase");
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const uppercaseEl = document.getElementById("uppercase");
+const lowercaseEl = document.getElementById("lowercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
 
-const characters = {
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  number: "0123456789",
-  symbol: "^!$%&|[](){}:;.,*+=#@<>~",
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
 };
 
-let previousSliderValue = lengthSlider.value;
+clipboardEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
 
-const generatePassword = () => {
-  let staticPassword = "";
-  let randomPassword = "";
-  let exclude = false;
-  let headLength = previousSliderValue;
-
-  options.forEach((option) => {
-    if (option.checked) {
-      if (option.id !== "exclude" && option.id !== "include") {
-        staticPassword += characters[option.id];
-      } else if (option.id === "include") {
-        staticPassword += ` ${staticPassword} `;
-      } else {
-        exclude = true;
-      }
-    }
-  });
-
-  for (let i = 0; i < headLength; i++) {
-    let randomChar =
-      staticPassword[Math.floor(Math.random() * staticPassword.length)];
-    if (exclude) {
-      !randomPassword.includes(randomChar) || randomChar === " "
-        ? (randomPassword += randomChar)
-        : i--;
-    } else {
-      randomPassword += randomChar;
-    }
+  if (!password) {
+    return;
   }
-  passwordResult.value = randomPassword;
-};
 
-const updateSlider = () => {
-  document.querySelector(".head_length span").innerHTML = lengthSlider.value;
-  previousSliderValue = lengthSlider.value;
-};
-
-updateSlider();
-
-const copyPassword = () => {
-  if (passwordResult.value !== "") {
-    navigator.clipboard.writeText(passwordResult.value);
-    const errorDiv = document.querySelector(".error");
-    errorDiv.style.display = "block";
-    errorDiv.textContent = "Password copied!";
-    errorDiv.style.background = "";
-    copyIcon.innerHTML = '<i class="fas fa-check"></i>';
-
-    setTimeout(() => {
-      errorDiv.style.display = "none";
-      copyIcon.innerHTML = '<i class="far fa-clipboard"></i>';
-    }, 2000);
-  } else {
-    const errorDiv = document.querySelector(".error");
-    errorDiv.textContent = "No password to copy!";
-    errorDiv.style.display = "block";
-    errorDiv.style.background = "red";
-    setTimeout(() => {
-      errorDiv.style.display = "none";
-    }, 2000);
-  }
-};
-
-lowercaseCheckbox.addEventListener("click", () => {
-  if (!lowercaseCheckbox.checked) {
-    const alertMessage = document.createElement("div");
-    alertMessage.classList.add("alert-message");
-    alertMessage.textContent = "Lowercase option cannot be unchecked!";
-
-    document.body.appendChild(alertMessage);
-
-    lowercaseCheckbox.checked = true;
-
-    setTimeout(() => {
-      alertMessage.remove();
-    }, 2000);
-  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+  alert("Password copied to clipboard!");
 });
 
-copyIcon.addEventListener("click", copyPassword);
-lengthSlider.addEventListener("input", updateSlider);
-generateBtn.addEventListener("click", generatePassword);
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
